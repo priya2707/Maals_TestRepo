216435360ea17225cb8bac43a6399f3da5d2c557
Maalini <maals93@gmail.com>
2020-12-05 17:38:08 +0530

    added addcheck with main method

--- TestJavaProjects/src/com/maalini/sample/AddCheck.java
+++ TestJavaProjects/src/com/maalini/sample/AddCheck.java
@@ -1,5 +1,11 @@
 package com.maalini.sample;
 
 public class AddCheck {
+	public static void main(String args[]) {
+		int a = 5;
+		int b = 10;
+		if(a + b >= 15)
+			System.out.println("you are permitted !!");
+	}
 
 }
